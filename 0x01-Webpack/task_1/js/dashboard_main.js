5638ec00b1135ad905c3d9f3ba7b23e85668ce69
sevajal <sevajal@yahoo.com>
2023-05-04 19:18:49 -0500

    Task 1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,23 @@
-import $ from "jquery"
-import _ from "lodash"
+const $ = require("jquery");
+const _ = require("lodash");
 
-$("body").append(`
-  <p>Holberton Dashboard</p>
-  <p>Dashboard data for the students</p>
-  <button>Click here to get started</button>
-  <p id='count'></p>
-  <p>Copyright - Holberton School</p>`)
+let count = 0;
 
 function updateCounter() {
-  let count = parseInt($("#count").html());
-  if (count) {
-    count += 1;
-  } else {
-    count = 1;
-  }
-  $("#count").empty()
-  $("#count").append(count);
+  count++;
+  $("#count").text(`${count} clicks on the button`);
 }
 
-$("button").on("click", _.debounce(updateCounter, 500));
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce(updateCounter, 500, {
+    leading: true,
+    trailing: false,
+  })
+);
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append($button);
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");

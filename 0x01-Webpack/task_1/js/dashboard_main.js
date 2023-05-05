ddaf9220bb34e00abcb5a5aae0e1616462f548d2
sevajal <sevajal@yahoo.com>
2023-05-04 19:12:02 -0500

    Task 1

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from "jquery"
+import _ from "lodash"
+
+$("body").append(`
+  <p>Holberton Dashboard</p>
+  <p>Dashboard data for the students</p>
+  <button>Click here to get started</button>
+  <p id='count'></p>
+  <p>Copyright - Holberton School</p>`)
+
+function updateCounter() {
+  let count = parseInt($("#count").html());
+  if (count) {
+    count += 1;
+  } else {
+    count = 1;
+  }
+  $("#count").empty()
+  $("#count").append(count);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
